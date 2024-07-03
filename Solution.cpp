70e304979c38f9b7c63364074ed839d4a86b6b8f
khalilFarhani <khalilfarhany@gmail.com>
2024-07-03 13:47:23 +0100

    Problem 144 : LeetCode 3179. Find the N-th Value After K Seconds

--- Solution.cpp
+++ Solution.cpp
@@ -3173,6 +3173,24 @@ int islandPerimeter(vector<vector<int>>& grid) {
        return ans;
    }
 
+
+   // problem 144 : You are given two integers n and k.
+   // Initially, you start with an array a of n integers where a[i] = 1 for all 0 <= i <= n - 1. After each second, you simultaneously update each element to be the sum of all its preceding elements plus the element itself.For example, after one second, a[0] remains the same, a[1] becomes a[0] + a[1], a[2] becomes a[0] + a[1] + a[2], and so on.
+   // Return the value of a[n - 1] after k seconds. Since the answer may be very large, return it modulo 109 + 7.
+   int valueAfterKSeconds(int n, int k) {
+       vector< long long> v(n, 1);
+       const  long long MOD = 1000000007;
+
+       for (int i = 0; i < k; i++) {
+           long long sum = 1;
+           for (int j = 1; j < n; j++) {
+               sum = (sum + v[j]) % MOD;
+               v[j] = sum;
+           }
+       }
+       return v[n - 1];
+   }
+
 };
 
 
