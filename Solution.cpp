01cca1649be1820a15381ff0be79f286ed5ef798
khalilFarhani <khalilfarhany@gmail.com>
2024-07-05 00:38:01 +0100

    Problem 145 : 2181. Merge Nodes in Between Zeros

--- Solution.cpp
+++ Solution.cpp
@@ -3191,6 +3191,35 @@ int islandPerimeter(vector<vector<int>>& grid) {
        return v[n - 1];
    }
 
+   // problem 145 : You are given the head of a linked list, which contains a series of integers separated by 0's. The beginning and end of the linked list will have Node.val == 0.
+   // For every two consecutive 0's, merge all the nodes lying in between them into a single node whose value is the sum of all the merged nodes. The modified list should not contain any 0's.
+   // Return the head of the modified linked list.
+   ListNode* mergeNodes(ListNode* head) {
+       int sum = 0;
+       ListNode* ans = nullptr;
+       ListNode* last = nullptr;
+       ListNode* tmp = head->next;
+       while (tmp) {
+           if (tmp->val == 0) {
+               ListNode* nvl = new ListNode(sum);
+               if (ans == nullptr) {
+                   ans = nvl;
+               }
+               else {
+                   last->next = nvl;
+               }
+               last = nvl;
+               sum = 0;
+           }
+           else {
+               sum += tmp->val;
+           }
+           tmp = tmp->next;
+       }
+       return ans;
+
+   }
+
 };
 
 
