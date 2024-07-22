05ff4ddca4bea319360364cc66a0d3ab0d0721ed
khalilFarhani <khalilfarhany@gmail.com>
2024-07-22 12:23:11 +0100

    Problem 152 : codewars From A to Z

--- Solution.cpp
+++ Solution.cpp
@@ -3378,6 +3378,18 @@ int islandPerimeter(vector<vector<int>>& grid) {
        return ans;
    }
 
+   // problem 153 : Given a string indicating a range of letters, return a string which includes all the letters in that range, including the last letter.
+   // Note that if the range is given in capital letters, return the string in capitals also!
+   string gimme_the_letters(const string& sp)
+   {
+       string letters;
+       for (size_t i = sp[0]; i <= sp[2]; i++) {
+           letters += i;
+       }
+
+       return letters;
+   }
+
 };
 
 
