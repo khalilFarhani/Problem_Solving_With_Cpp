08c35fd9ef564027af7d9343ff36a9898b60f58c
khalilFarhani <khalilfarhany@gmail.com>
2024-07-23 14:15:52 +0100

    Problem 154 : Leetcode 1636. Sort Array by Increasing Frequency

--- Solution.cpp
+++ Solution.cpp
@@ -3390,6 +3390,24 @@ int islandPerimeter(vector<vector<int>>& grid) {
        return letters;
    }
 
+   // problem 154 : Given an array of integers nums, sort the array in increasing order based on the frequency of the values. If multiple values have the same frequency, sort them in decreasing order.
+   // Return the sorted array.
+   vector<int> frequencySort(vector<int>& nums) {
+       unordered_map<int, int> freq;
+       for (int num : nums) {
+           freq[num]++;
+       }
+
+       sort(nums.begin(), nums.end(), [&](int a, int b) {
+           if (freq[a] == freq[b]) {
+               return a > b;
+           }
+           return freq[a] < freq[b];
+           });
+
+       return nums;
+   }
+
 };
 
 
