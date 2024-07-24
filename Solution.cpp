fc1588bfe673b857b1989d9f53277901d81e90d3
khalilFarhani <khalilfarhany@gmail.com>
2024-07-24 10:29:47 +0100

    Problem 156 : Codewars Alphabet symmetry

--- Solution.cpp
+++ Solution.cpp
@@ -3432,6 +3432,21 @@ int islandPerimeter(vector<vector<int>>& grid) {
        return ans;
    }
 
+   // problem 156 : Consider the word "abode". We can see that the letter a is in position 1 and b is in position 2. In the alphabet, a and b are also in positions 1 and 2. Notice also that d and e in abode occupy the positions they would occupy in the alphabet, which are positions 4 and 5.
+   // Given an array of words, return an array of the number of letters that occupy their positions in the alphabet for each word
+   vector<int> solve(const vector<string>& arr) {
+       vector<int>ans;
+       for (const string& str : arr) {
+           int count = 0;
+           for (int i = 0; i < str.length(); i++) {
+               if (toupper(str[i]) - 65 == i)
+                   count++;
+           }
+           ans.push_back(count);
+       }
+       return ans;
+   };
+
 };
 
 
