9064d670de34bbd18027e2546c187555ff4b45f0
khalilFarhani <khalilfarhany@gmail.com>
2025-01-20 22:34:26 +0100

    Problem 169 : LeetCode 3423. Maximum Difference Between Adjacent Elements in a Circular Array

--- Solution.cpp
+++ Solution.cpp
@@ -3661,6 +3661,17 @@ int islandPerimeter(vector<vector<int>>& grid) {
    }
 
 
+   //problem 169 : leetcode 3423. Maximum Difference Between Adjacent Elements in a Circular Array
+   int maxAdjacentDistance(vector<int>& nums) {
+       int n = nums.size();
+       int ans = abs(nums[0] - nums[n - 1]);
+       for (int i = 1; i < n; i++) {
+           ans = max(ans, abs(nums[i] - nums[i - 1]));
+       }
+       return ans;
+   }
+
+
 };
 
 
